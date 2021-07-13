2e7b0188914b6af8d21b4fac6588e546310a9ca7
Shan Wijenayaka <shanwijenayaka@outlook.com>
2021-07-14 03:50:44 +0530

    server set to custom instance

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 	"web-app-analyser-service/handlers"
 )
 
@@ -14,6 +17,27 @@ func main() {
 	serveMux := http.NewServeMux()
 	serveMux.Handle("/", webUrlHandler)
 
-	http.ListenAndServe(":8080", serveMux)
+	server := &http.Server{
+		Addr:        ":8080",
+		Handler:     serveMux,
+		IdleTimeout: 300 * time.Second,
+		ReadTimeout: 120 * time.Second,
+	}
 
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			logger.Fatal(err)
+		}
+	}()
+
+	signalChannel := make(chan os.Signal)
+	signal.Notify(signalChannel, os.Interrupt)
+	signal.Notify(signalChannel, os.Kill)
+
+	sig := <-signalChannel
+	log.Println("Received terminate signal, shutting down : ", sig)
+
+	timeoutContext, _ := context.WithDeadline(context.Background(), time.Now().Add(30*time.Second))
+	server.Shutdown(timeoutContext)
 }
